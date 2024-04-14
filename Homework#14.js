ce8014918b69e106fc4943134f4df3877f24b2b4
luuuuckyyyyy <166928371+luuuuckyyyyy@users.noreply.github.com>
2024-04-14 20:31:36 +0300

    Create Homework#14.js
    Бот для яндекс (почти рабочий)

---
+++ Homework#14.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         My first script
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       Makarov Maxim
+// @match        https://ya.ru/*
+// @grant        none
+// ==/UserScript==
+
+
+
+let button = document.querySelectorAll("button[class$='button']")[0];
+let input = document.getElementsByClassName("search3__input mini-suggest__input")[0];
+let inputed = document.getElementsByClassName("HeaderDesktopForm-Input mini-suggest__input")[0];
+
+
+let links = document.links;
+
+
+let keywords = ["10 самых популярных шрифтов от Google"]
+let keyword = keywords[getRandom(0, keywords.length)];
+
+
+if (input != null){
+    input.value = keyword;
+    button.click();
+} else {
+    for (let i = 0; i < links.length; i++){
+        if (links[i].href.indexOf("uguide.ru") != -1){
+            let link = links[i];
+            link.click();
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
